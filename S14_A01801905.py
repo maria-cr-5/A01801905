6b6db8a6c016946be96b45e91dff89e206193780
maria-cr-5 <a01801905@tec.mx>
2025-09-19 07:53:32 -0600

    S14_A01801905.py

--- S14_A01801905.py
+++ S14_A01801905.py
@@ -4,3 +4,13 @@ if age < 0:
 if age < 18:
  print("Valid age")   
   
+age = int(input("Please enter your age:"))
+if 13 <= age and age <= 19:
+  print("You are a teenager")
+else:
+ print("You are not a teenager")
+
+age_input = int(input("Please enter your age: "))
+actual_year = 2025
+birth_year_approx = actual_year - age_input
+print(birth_year_approx)
